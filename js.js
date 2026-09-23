877ac0655cfb6f85e415facda80588652fc663e3
just-a-danya <shramko007ru@mail.ru>
2026-09-23 18:19:55 +0400

    Update js.js

--- js.js
+++ js.js
@@ -1,24 +1,17 @@
 const members = document.querySelectorAll('.memberImage');
 
 members.forEach(member => {
-
     const mainImage = member.querySelector('.mainImage');
 
     mainImage.addEventListener('click', function(event) {
-
         event.stopPropagation();
 
         member.classList.toggle('active');
-
     });
-
 });
 
-
 document.addEventListener('click', function() {
-
     members.forEach(member => {
         member.classList.remove('active');
     });
-
 });
